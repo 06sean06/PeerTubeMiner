a0154771625761f4ef6e0789eae5f14562091c2a
Laura Carmona <laucarmor@alum.us.es>
2026-04-13 17:02:40 +0200

    imports

--- src/main/java/aiss/PeerTube/PeerTubeApplication.java
+++ src/main/java/aiss/PeerTube/PeerTubeApplication.java
@@ -2,6 +2,10 @@ package aiss.PeerTube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+ 
 
 @SpringBootApplication
 public class PeerTubeApplication {
