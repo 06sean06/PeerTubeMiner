24311678371b0a67e4c75d74592d60269dba8af1
carrotluc <lucicarcam@gmail.com>
2026-04-20 19:01:29 +0200

    errors solved

--- src/main/java/aiss/PeerTube/modelPT/comment/AccountPT.java
+++ src/main/java/aiss/PeerTube/modelPT/comment/AccountPT.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import aiss.PeerTube.model.channel.AvatarPT;
+import aiss.PeerTube.modelPT.channel.AvatarPT;
 
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
