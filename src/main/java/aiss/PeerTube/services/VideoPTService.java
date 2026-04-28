18969dfc6eeaefec69b74f329a5b2eef0c51791f
carrotluc <lucicarcam@gmail.com>
2026-04-28 13:08:34 +0200

    video service and service test (+ wrapper)

--- src/main/java/aiss/PeerTube/services/VideoPTService.java
+++ src/main/java/aiss/PeerTube/services/VideoPTService.java
@@ -1,5 +1,49 @@
 package aiss.PeerTube.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import aiss.PeerTube.modelPT.video.VideoPT;
+import aiss.PeerTube.modelPT.video.VideoPTResponse;
+
+@Service
 public class VideoPTService {
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${peertube.url}")
+    private String url;
+
+    // GET https://peertube3.cpy.re/api/v1/videos
+    public List<VideoPT> findAllVideos() {
+        String uri = url + "/videos"; 
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<VideoPT> request = new HttpEntity<>(headers);
+        @SuppressWarnings("null") // Lo he añadido porque VSCode cree que HttpMethod.GET es null, pero no lo es, es un enum.
+        ResponseEntity<VideoPTResponse> response = restTemplate.exchange(uri, HttpMethod.GET, request, VideoPTResponse.class);
+        VideoPTResponse body = response.getBody();
+        if (body == null || body.getData() == null) {
+            return List.of();
+        }
+        return body.getData();
+    }
+
+    //GET https://peertube3.cpy.re/api/v1/videos/5JoZUZzbUdpNqGXpd5QJ5T
+    public VideoPT findVideoById(String idVideo) {
+        String uri = url + "/videos/" + idVideo;
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<VideoPT> request = new HttpEntity<>(headers);
+        @SuppressWarnings("null") // Lo he añadido porque VSCode cree que HttpMethod.GET es null, pero no lo es, es un enum.
+        ResponseEntity<VideoPT> response = restTemplate.exchange(uri, HttpMethod.GET, request, VideoPT.class);
+        return response.getBody();
+    }
 }
---
+++ src/test/java/aiss/PeerTube/services/VideoPTServiceTest.java
@@ -0,0 +1,62 @@
+package aiss.PeerTube.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import aiss.PeerTube.modelPT.video.VideoPT;
+
+@SpringBootTest
+public class VideoPTServiceTest {
+
+    @Autowired
+    VideoPTService videoPTService;
+
+    @Test
+    @DisplayName("Get all videos")
+    void testFindAllVideos() {
+        //GET https://peertube3.cpy.re/api/v1/videos
+        List<VideoPT> videos = videoPTService.findAllVideos();
+        assertFalse(videos.isEmpty(), "The videos list should contain at least one video");
+
+        VideoPT first = videos.get(0);
+        assertNotNull(videos, "The videos list should not be null");
+        assertNotNull(first, "First video should not be null");
+        assertNotNull(first.getLanguage(), "Language object should not be null");
+        assertNotNull(first.getUrl(), "Video URL should not be null");
+        assertNotNull(first.getCategory(), "Category object should not be null");
+
+        assertEquals(444381, first.getId(), "The first video ID should match the expected value");
+        assertEquals("8yUbMhzraebRoyRPJbHNVX", first.getShortUUID(), "The shortUUID should match");
+        assertEquals("GRISE BOUILLE TV #72 — Indiana Jones & la chute à vélo",
+            first.getName(), "The video name should match");
+        assertEquals("French", first.getLanguage().getLabel(), "Language label should be French");
+        assertEquals("Art", first.getCategory().getLabel(), "Category label should be Art");
+}
+
+
+    @Test
+    @DisplayName("Get video by id")
+    void testFindVideoById() {
+        // GET https://peertube3.cpy.re/api/v1/videos/5JoZUZzbUdpNqGXpd5QJ5T
+        VideoPT video = videoPTService.findVideoById("5JoZUZzbUdpNqGXpd5QJ5T");
+
+        assertNotNull(video, "The video should not be null");
+        assertNotNull(video.getDuration(), "Video duration should not be null");
+        assertNotNull(video.getLanguage(), "Video language should not be null");
+
+        assertTrue(video.getDuration() > 0, "Video duration should be greater than zero");
+
+        assertEquals("English", video.getLanguage().getLabel(),
+                "The video language should be English");
+        assertEquals("5JoZUZzbUdpNqGXpd5QJ5T", video.getShortUUID(),
+                "The shortUUID should match the requested video ID");
+        assertEquals("Agent 327: Operation Barbershop", video.getName(),
+                "The video name should match the expected title");
+    }
+}
