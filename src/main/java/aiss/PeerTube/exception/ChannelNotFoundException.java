73125b5a011349c6c7945ce8bd90ef1048aef130
carrotluc <lucicarcam@gmail.com>
2026-04-28 12:41:21 +0200

    i mean, exceptions**

--- src/main/java/aiss/PeerTube/exception/ChannelNotFoundException.java
+++ src/main/java/aiss/PeerTube/exception/ChannelNotFoundException.java
@@ -3,7 +3,7 @@ package aiss.PeerTube.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code=HttpStatus.NOT_FOUND, reason= "Video not found")
+@ResponseStatus(code=HttpStatus.NOT_FOUND, reason= "Channel not found")
 public class ChannelNotFoundException extends Exception{
 
 }
