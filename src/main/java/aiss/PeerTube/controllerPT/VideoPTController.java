48fe5439bee8a2fc086f7ddd70fefa4d51e6df48
carrotluc <lucicarcam@gmail.com>
2026-04-28 13:19:45 +0200

    video repository and controller (it worked w postman)

---
+++ src/main/java/aiss/PeerTube/controllerPT/VideoPTController.java
@@ -0,0 +1,34 @@
+package aiss.PeerTube.controllerPT;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import aiss.PeerTube.exception.VideoNotFoundException;
+import aiss.PeerTube.modelPT.video.VideoPT;
+import aiss.PeerTube.repositoryPT.VideoPTRepository;
+
+@RestController
+@RequestMapping("PeerTube/videos")
+public class VideoPTController {
+
+    private VideoPTRepository videoRepository;
+
+    public VideoPTController(VideoPTRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    // GET http://localhost:8080/PeerTube/videos
+    @GetMapping
+    public List<VideoPT> getVideos() {
+        return videoRepository.findAllVideos();
+    }
+    // GET http://localhost:8080/PeerTube/videos/{idVideo}
+    @GetMapping("/{idVideo}")
+    public VideoPT getVideoById(@PathVariable String idVideo) throws VideoNotFoundException {
+        return videoRepository.findVideoById(idVideo);
+    }
+}
---
+++ src/main/java/aiss/PeerTube/repositoryPT/VideoPTRepository.java
@@ -0,0 +1,26 @@
+package aiss.PeerTube.repositoryPT;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import aiss.PeerTube.modelPT.video.VideoPT;
+import aiss.PeerTube.services.VideoPTService;
+
+@Repository
+public class VideoPTRepository {
+
+    @Autowired
+    VideoPTService videoPTService;
+
+    public List<VideoPT> findAllVideos() {
+        List<VideoPT> videos = videoPTService.findAllVideos();
+        return videos;
+    }
+    public VideoPT findVideoById(String idVideo) {
+        VideoPT video = videoPTService.findVideoById(idVideo);
+        return video;
+    }
+
+}
