801c945df9be603e1fa6689bb02747a24d53799e
carrotluc <lucicarcam@gmail.com>
2026-05-05 13:14:40 +0200

    ya no da error

--- src/main/java/aiss/PeerTube/repositoryPT/CommentPTRepository.java
+++ src/main/java/aiss/PeerTube/repositoryPT/CommentPTRepository.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import aiss.PeerTube.model.modelPT.comment.CommentBasePT;
-import aiss.PeerTube.model.modelPT.comment.CommentPTResponse;
 import aiss.PeerTube.model.modelPT.comment.CommentThreadPT;
 import aiss.PeerTube.services.CommentPTService;
 
