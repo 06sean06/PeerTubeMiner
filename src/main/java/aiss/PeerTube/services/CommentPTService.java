1ff7aa1a0061abd201133badeec4083e6eca9f1b
carrotluc <lucicarcam@gmail.com>
2026-05-09 23:56:52 +0200

    0 problems

--- src/main/java/aiss/PeerTube/services/CommentPTService.java
+++ src/main/java/aiss/PeerTube/services/CommentPTService.java
@@ -47,14 +47,14 @@ public class CommentPTService {
 
     // Dado un idVideo y un idComment, te devuelve el comentario de ese video en específico. 
     // GET https://peertube3.cpy.re/api/v1/videos/{id}/comment-threads/{threadId}
+    @SuppressWarnings("null")
     public CommentThreadPT getSpecificComment(String videoId, String threadId) {
         String uri = url + "/videos/" + videoId + "/comment-threads/" + threadId;
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<Void> request = new HttpEntity<>(headers);
-
-        try {    
-        ResponseEntity<CommentThreadPT> response = restTemplate.exchange(uri, HttpMethod.GET, request, CommentThreadPT.class);
-        return response.getBody();
+    try {    
+            ResponseEntity<CommentThreadPT> response = restTemplate.exchange(uri, HttpMethod.GET, request, CommentThreadPT.class);
+            return response.getBody();
     } catch (org.springframework.web.client.HttpClientErrorException.NotFound e) {     // captura de error 
         return null;
     } catch (Exception e) {
